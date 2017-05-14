b9e8122dd6002cfa7565b7e1157a5f7c99457c16
bnuckles <nuckles26@fullerton.edu>
2017-05-14 01:22:32 -0700

    Loaded data into index route
    routes/index.js loads data into ember variables galaxy and iphone,
    example of usage in index.hbs

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      galaxy: this.store.findAll('galaxy'),
+      iphone: this.store.findAll('iphone')
+    })
+  },
+
+  setupController(controller, models) {
+    controller.set('galaxy', models.galaxy);
+    controller.set('iphone', models.iphone);
+  }
 });
