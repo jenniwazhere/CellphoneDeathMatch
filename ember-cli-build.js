7ff586d93aa67ca264fd465f3e48c8422158ad1d
Jennifer Kang <jennikang@csu.fullerton.edu>
2017-05-15 16:16:06 -0700

    added only the text from the front-end page

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,23 +2,33 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
-  });
+module.exports = function (defaults) {
+    var bootstrapPath = 'bower_components/bootstrap-sass/assets/';
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+    var app = new EmberApp(defaults, {
+        sassOptions: {
+            includePaths: [
+bootstrapPath + 'stylesheets'
+]
+        }
+    });
+    // ... Template comments ...
+    // Create paths to bootstrap assets
+    // Add assets to app with import
+    app.import(bootstrapPath + 'javascripts/bootstrap.js');
 
-  return app.toTree();
+    // Use `app.import` to add additional libraries to the generated
+    // output files.
+    //
+    // If you need to use different assets in different
+    // environments, specify an object as the first parameter. That
+    // object's keys should be the environment name and the values
+    // should be the asset to use in that environment.
+    //
+    // If the library that you are including contains AMD or ES6
+    // modules that you would like to import into your application
+    // please specify an object with the list of modules as keys
+    // along with the exports of each module as its value.
+
+    return app.toTree();
 };
